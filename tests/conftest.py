7a6ba0d256504e3db654ef28c6789f741d35feb5
Susana Villegas <suevillegasc@gmail.com>
2025-08-10 10:23:52 -0400

    add

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+#aqui van a estar todos los fixtures, se definen
+from time import sleep
+
+import pytest
+from utils_movies import create_driver
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--headless",
+        action="store_true",
+        help="Ejecutar pruebas en modo headless (sin interfaz de usuario)"
+    )
+
+@pytest.fixture
+def driver(request):
+    headless = request.config.getoption("--headless")
+    driver = create_driver(headless=headless)
+    yield driver
+    sleep(3)
+    driver.quit()
