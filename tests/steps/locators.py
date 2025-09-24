d5ca7fce88289c2c64bf0122ef611a17e825e75c
Susana <suevillegasc@gmail.com>
2025-09-24 20:42:41 -0300

    se agrega archivo de locators centralizado

---
+++ tests/steps/locators.py
@@ -0,0 +1,34 @@
+# Locators for web elements used in automated tests
+
+# 🔹 Homepage
+NAV_ALIMENTOS_BUTTON_XPATH = '//a[contains(@href, "/alimentos")]'
+NAV_PROMOS_BUTTON_XPATH = '//a[contains(@href, "/promos")]'
+NAV_HOME_LINK_HREF_XPATH = '//a[@href="/"]'
+
+# 🔹 Sección Alimentos
+PALOMITAS_IMAGE_XPATH = '//img[@alt="Palomitas"]'
+HOT_DOG_IMAGE_XPATH = '//img[@alt="Hot Dog"]'
+NACHOS_IMAGE_XPATH = '//img[@alt="Nachos"]'
+PIZZA_IMAGE_XPATH = '//img[@alt="Pizza por rebanada"]'
+BURGER_IMAGE_XPATH = '//img[@alt="Hamburguesa Clásica"]'
+ICECREAM_IMAGE_XPATH = '//img[@alt="Helado Sundae"]'
+PRETZEL_IMAGE_XPATH = '//img[@alt="Pretzel Jumbo"]'
+SANDWICH_IMAGE_XPATH = '//img[@alt="Sandwich de Pollo"]'
+DULCES_IMAGE_XPATH = '//img[@alt="Dulces Surtidos"]'
+AGUA_EMBOTELLADA_IMAGE_XPATH = '//img[@alt="Agua Embotellada"]'
+
+# 🔹 Sección detalle de Alimentos y Promos
+# Este botón se reutiliza en todas las vistas de detalle de alimentos y promos
+ADD_TO_CART_BUTTON_XPATH = '//button[contains(text(), "Agregar al carrito")]'
+
+# 🔹 Sección Promos
+COMBO_PAREJA_IMAGE_XPATH = '//img[@alt="Combo Pareja"]'
+MIERCOLES_2X1_IMAGE_XPATH = '//img[@alt="Miércoles 2x1"]'
+COMBO_KIDS_IMAGE_XPATH = '//img[@alt="Combo Kids"]'
+TARJETA_MEMBRESIA_IMAGE_XPATH = '//img[@alt="Tarjeta Membresía"]'
+DESCUENTO_MATINE_IMAGE_XPATH = '//img[@alt="Descuento Matiné"]'
+COMBO_FAMILIAR_IMAGE_XPATH = '//img[@alt="Combo Familiar"]'
+PRECIO_ESTUDIANTES_IMAGE_XPATH = '//img[@alt="Precio Estudiantes"]'
+COMBO_AMIGOS_IMAGE_XPATH = '//img[@alt="Combo Amigos"]'
+PALOMITAS_REFILL_IMAGE_XPATH = '//img[@alt="Palomitas Refill"]'
+BEBIDA_GRANDE_GRATIS_IMAGE_XPATH = '//img[@alt="Bebida Grande Gratis"]'
